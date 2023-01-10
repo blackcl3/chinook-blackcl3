57c90b6654dcbaa9b6abab68f14c4c55b01a6737
Christian Black <blackl3@gmail.com>
2023-01-09 19:50:03 -0600

    top 2009 agent file completed

---
+++ top_2009_agent.sql
@@ -0,0 +1,15 @@
+SELECT
+   STRFTIME('%Y', Invoice.InvoiceDate) InvoiceYear,
+   Employee.FirstName, 
+   Employee.LastName,
+   SUM(Invoice.Total) 
+FROM Employee
+INNER JOIN Customer on Employee.EmployeeId = Customer.SupportRepId INNER JOIN Invoice on Customer.SupportRepId = Invoice.CustomerId
+WHERE InvoiceDate 
+    BETWEEN '2009-01-01' AND '2009-12-31'
+GROUP BY
+   EmployeeId
+ORDER BY
+    SUM(Invoice.Total) DESC
+LIMIT
+    1
